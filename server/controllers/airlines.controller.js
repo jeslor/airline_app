5abd27af1868eb21ddac6dcdf6989a3229921c0b
Jeslor Ssozi <jbestius@gmail.com>
2025-07-25 13:13:55 +0200

    add the prisma import in the airline controller

--- server/controllers/airlines.controller.js
+++ server/controllers/airlines.controller.js
@@ -1,7 +1,13 @@
 import asyncWrapper from "../utils/asyncWrapper";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
 const getAirlines = asyncWrapper(async (req, res) => {
-  const airlines = await Airline.find({});
+  const airlines = await prisma.airline.findMany({
+    orderBy: {
+      name: "asc",
+    },
+  });
   res.status(200).json({
     status: "success",
     data: {
