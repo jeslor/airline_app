618387fc9cd24ae7e4c8150255effcd63850a29d
Jeslor Ssozi <jbestius@gmail.com>
2025-07-27 13:47:58 +0200

    add the sky

--- client/src/components/Globe/GlobeWrapper.tsx
+++ client/src/components/Globe/GlobeWrapper.tsx
@@ -1,9 +1,15 @@
 import GlobeWithMultiplePlanes from "./Globe";
+import SpaceCanvas from "./SpaceCanvas";
 
 const GlobeWrapper = () => {
   return (
-    <div className="h-screen ">
-      <GlobeWithMultiplePlanes />
+    <div className="h-screen relative">
+      <div className="absolute inset-0 z-[2]">
+        <GlobeWithMultiplePlanes />
+      </div>
+      <div className="relative z-[1]">
+        <SpaceCanvas />
+      </div>
     </div>
   );
 };
