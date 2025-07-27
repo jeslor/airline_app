4e6de34c56913863c6044f9dec82a2f784180b21
Jeslor Ssozi <jbestius@gmail.com>
2025-07-27 13:43:29 +0200

    update the globe map

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import GlobeWithPlane from "./components/Globe";
 
 const App = () => {
   return (
-    <div className="h-screen bg-gray-600">
+    <div className="h-screen">
       <GlobeWithPlane />
     </div>
   );
