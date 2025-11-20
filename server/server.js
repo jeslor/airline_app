db6f5a0619e499a8847fb9013d91943f3a1a9810
Jeslor Ssozi <jbestius@gmail.com>
2025-11-20 09:45:16 +0100

    fix puppetree and cross origin errors

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // or your domain
+  res.header("Access-Control-Allow-Origin", "*"); // your domain or specific origins
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
