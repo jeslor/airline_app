d354795ef79b875825c6bf10e6f59e6b42c74d2e
Jeslor Ssozi <jbestius@gmail.com>
2025-11-25 11:59:05 +0100

    update the generate pdf logic to work on vercel

--- server/utils/pdfHelper.js
+++ server/utils/pdfHelper.js
@@ -1,32 +1,21 @@
 import chromium from "chrome-aws-lambda";
 import puppeteer from "puppeteer-core";
-import path from "path";
-import os from "os";
 
 export const generatePDF = async (htmlContent, options = {}) => {
   let browser = null;
-
   try {
-    const isProd = process.env.NODE_ENV === "production";
-
-    const executablePath = isProd
-      ? await chromium.executablePath // works on Vercel
-      : getLocalChromePath(); // must provide local path in dev
-
     browser = await puppeteer.launch({
-      args: isProd
-        ? chromium.args.concat([
-            "--no-sandbox",
-            "--disable-setuid-sandbox",
-            "--disable-dev-shm-usage",
-            "--disable-accelerated-2d-canvas",
-            "--no-first-run",
-            "--no-zygote",
-            "--single-process",
-          ])
-        : [],
+      args: chromium.args.concat([
+        "--no-sandbox",
+        "--disable-setuid-sandbox",
+        "--disable-dev-shm-usage",
+        "--disable-accelerated-2d-canvas",
+        "--no-first-run",
+        "--no-zygote",
+        "--single-process",
+      ]),
       defaultViewport: chromium.defaultViewport,
-      executablePath,
+      executablePath: await chromium.executablePath,
       headless: true,
     });
 
@@ -39,10 +28,10 @@ export const generatePDF = async (htmlContent, options = {}) => {
     const pdfBuffer = await page.pdf({
       format: "A4",
       margin: {
-        top: "10px",
-        right: "8px",
-        bottom: "10px",
-        left: "8px",
+        top: "20px",
+        bottom: "20px",
+        left: "14px",
+        right: "14px",
       },
       printBackground: true,
       ...options,
@@ -53,18 +42,3 @@ export const generatePDF = async (htmlContent, options = {}) => {
     if (browser) await browser.close();
   }
 };
-
-// Helper function: find local Chrome path
-function getLocalChromePath() {
-  const platform = os.platform();
-
-  if (platform === "win32") {
-    return "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe";
-  } else if (platform === "darwin") {
-    return "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome";
-  } else if (platform === "linux") {
-    return "/usr/bin/google-chrome"; // adjust if needed
-  } else {
-    throw new Error("Unsupported OS for local Chrome");
-  }
-}
